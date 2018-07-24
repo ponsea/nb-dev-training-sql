81febcefb41308845283925621bd95a13305ec15
ponsea <puodsea@gmail.com>
2018-07-24 17:50:11 +0900

    A-49~58の誤答の修正

--- theme-a/51.sql
+++ theme-a/51.sql
@@ -1,2 +1,5 @@
-SELECT COUNT(更新日)
+# 誤り: SELECT COUNT(更新日)
+SELECT COUNT(*) - COUNT(更新日)
 FROM 口座;
+
+# 問題履き違えてた
--- theme-a/57.sql
+++ theme-a/57.sql
@@ -1,4 +1,5 @@
-SELECT 種別, SUM(残高)
+# 誤り: SELECT 種別, SUM(残高)
+SELECT 種別, SUM(残高), COUNT(*)
 FROM 口座
 GROUP BY 種別
 HAVING SUM(残高) > 3000000;
