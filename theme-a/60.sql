a8167eca9b55b800f05fde8a425a2b21747e05f6
ponsea <puodsea@gmail.com>
2018-07-25 17:09:13 +0900

    A-60の回答

---
+++ theme-a/60.sql
@@ -0,0 +1,14 @@
+SELECT
+  残高,
+  (
+    SELECT SUM(入金額)
+    FROM 取引
+    WHERE 口座番号 = '1115600' AND 日付 = '2012-12-28'
+  ) AS 入金額,
+  (
+    SELECT SUM(出金額)
+    FROM 取引
+    WHERE 口座番号 = '1115600' AND 日付 = '2012-12-28'
+  ) AS 入金額
+FROM 口座
+WHERE 口座番号 = '1115600';
