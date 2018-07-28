3e2eeff40e9bd3f53b98076ba8a622a573de738a
ponsea <puodsea@gmail.com>
2018-07-28 14:24:56 +0900

    A-65の回答

---
+++ theme-a/65.sql
@@ -0,0 +1,9 @@
+SELECT
+  K.口座番号,
+  TJ.取引事由名,
+  COALESCE(T.入金額, T.出金額) AS 取引金額
+FROM
+  口座 K INNER JOIN 取引 T      ON K.口座番号 = T.口座番号
+         INNER JOIN 取引事由 TJ ON T.取引事由ID = TJ.取引事由ID
+WHERE K.口座番号 IN ('0311240', '1234161', '1750902')
+ORDER BY K.口座番号, T.取引番号;
