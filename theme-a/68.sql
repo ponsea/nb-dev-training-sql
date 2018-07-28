05c19d462a8b808214f440f275823a7b163ef990
ponsea <puodsea@gmail.com>
2018-07-28 16:19:52 +0900

    A-65~75の誤答の修正

--- theme-a/68.sql
+++ theme-a/68.sql
@@ -7,3 +7,15 @@ UNION
 SELECT HK.口座番号, '解約済み' AS 名義, 0 AS 残高
 FROM 廃止口座 HK INNER JOIN 取引 T ON HK.口座番号 = T.口座番号
 WHERE T.日付 = '2011-03-01';
+
+# 間違ってはいないと思うが、参考書の回答例とかなり違っていた。
+# 回答例によると、廃止口座と対応付ける必要がなかった。
+# 以下、参考書の回答例
+# SELECT
+#   T.口座番号,
+#   COALESCE(K.名義, '解約済み') AS 名義,
+#   COALESCE(K.残高, 0) AS 残高
+# FROM
+#   取引 AS T LEFT JOIN 口座 AS K ON T.口座番号 = K.口座番号
+# WHERE
+#   T.日付 = '2011-03-01';
--- theme-a/75.sql
+++ theme-a/75.sql
@@ -17,3 +17,38 @@ WHERE
       )
   )
 ORDER BY K.名義, K.口座番号;
+
+# 回答例とだいぶ違っていた。
+# 以下回答例
+
+# /* 自己結合を用いた場合 */
+# SELECT DISTINCT
+#   K1.名義,
+#   K1.口座番号,
+#   K1.種別,
+#   K1.残高,
+#   K1.更新日
+# FROM 口座 AS K1 JOIN 口座 AS K2 ON K1.名義 = K2.名義
+# WHERE K1.口座番号 <> K2.口座番号
+# ORDER BY K1.名義, K1.口座番号;
+# 
+# /* 集計関数と結合を用いた場合 */
+# SELECT DISTINCT
+#   K1.名義,
+#   K1.口座番号,
+#   K1.種別,
+#   K1.残高,
+#   K1.更新日
+# FROM
+#   口座 AS K1
+#   JOIN
+#   (
+#     SELECT
+#       名義,
+#       COUNT(名義) AS 口座数
+#     FROM 口座
+#     GROUP BY 名義
+#     HAVING COUNT(名義) > 1
+#   ) AS K2
+#   ON K1.名義 = K2.名義
+# ORDER BY K1.名義, K1.口座番号;
