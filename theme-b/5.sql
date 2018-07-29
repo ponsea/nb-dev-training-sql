1b0e97e5cea6c5871925a01c5dbbb118f8bf07e2
ponsea <puodsea@gmail.com>
2018-07-29 15:28:09 +0900

    B-05の回答

---
+++ theme-b/5.sql
@@ -0,0 +1,3 @@
+INSERT INTO 商品 VALUES ('W0461', '冬のあったかコート', 12800, 1, NULL);
+INSERT INTO 商品 VALUES ('S0331', '春のさわやかコート', 6800, 1, NULL);
+INSERT INTO 商品 VALUES ('A0582', '秋のシックなコート', 9800, 1, NULL);
