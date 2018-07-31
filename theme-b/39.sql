f04a7b5b58b95a0e84b07c911278c16905f3b9d3
ponsea <puodsea@gmail.com>
2018-07-31 16:40:26 +0900

    B-39の回答

---
+++ theme-b/39.sql
@@ -0,0 +1,27 @@
+SELECT
+  商品コード,
+  商品名,
+  単価,
+  CASE
+    WHEN 単価 < 3000 THEN 'S'
+    WHEN 単価 BETWEEN 3000 AND 9999 THEN 'M'
+    WHEN 単価 > 10000 THEN 'L'
+    ELSE NULL
+  END AS 商品価格ランク,
+  CONCAT(
+    商品区分,
+    ':',
+    CASE 商品区分
+      WHEN '1' THEN '衣類'
+      WHEN '2' THEN '靴'
+      WHEN '3' THEN '雑貨'
+      WHEN '9' THEN '未分類'
+      ELSE NULL
+    END
+  ) AS 商品区分
+FROM
+  商品
+ORDER BY
+  単価,
+  商品コード
+;
