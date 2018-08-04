b3328b5da39aa0390d33ccbdf096faba97c62279
ponsea <puodsea@gmail.com>
2018-08-04 15:59:28 +0900

    C-43に回答例追記

--- theme-c/43.sql
+++ theme-c/43.sql
@@ -1,3 +1,5 @@
+# MySQLではCAST関数のキャスト先の型が数値の場合はSIGNEDやUNSIGNEDを指定する
+# INTEGERだと怒られた
 SELECT
   名称 AS なまえ,
   HP,
@@ -14,3 +16,13 @@ ORDER BY
   リスク値 DESC,
   HP
 ;
+
+# 回答例
+# SELECT 名称 AS なまえ, HP, 状態コード,
+#        CASE WHEN HP <= 50 THEN 3 + CAST(状態コード AS INTEGER)
+#             WHEN HP >= 51 AND HP <= 100 THEN 2 + CAST(状態コード AS INTEGER)
+#             WHEN HP >= 101 AND HP <= 150 THEN 1 + CAST(状態コード AS INTEGER)
+#             ELSE CAST(状態コード AS INTEGER)
+#        END AS リスク値
+#   FROM パーティー
+#  ORDER BY リスク値 DESC, HP
